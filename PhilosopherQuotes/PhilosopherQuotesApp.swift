c8f216b490b3b127a03f8d3db9b74be25b28e165
David-Simonetti-ND <dsimone2@nd.edu>
2022-08-14 11:59:02 -0400

    Initial Commit

---
+++ PhilosopherQuotes/PhilosopherQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhilosopherQuotesApp.swift
+//  PhilosopherQuotes
+//
+//  Created by David Simonetti on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PhilosopherQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
